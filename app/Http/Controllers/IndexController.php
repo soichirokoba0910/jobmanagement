dd79aacda986d1feda52b0f33ce27dc71d32cf7c
koba <kobasoichiro910@gmail.com>
2023-01-04 21:14:27 +0900

    カレンダー開発環境前

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,13 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\FlareClient\View;
 
 class IndexController extends Controller
 {
-    public function index(Request $request,$month){
-        $calendar = calendar($section, $patient, $month);
-        $month = new CarbonImmutable($month); 
-        return view('index.index',compact( 'calendar', 'month'));
+
+    public $month=10;
+    public function index(Request $request){
+        global $today,$monthNext,$yearNext,$monthPrev,$yearPrev;
+        $today = filter_input(INPUT_POST, 'today');
+        $monthNext = filter_input(INPUT_POST, 'monthNext');
+        $yearNext = filter_input(INPUT_POST, 'yearNext');
+        $monthPrev = filter_input(INPUT_POST, 'monthPrev');
+        $yearPrev = filter_input(INPUT_POST, 'yearPrev');
+        return view('index.index');
+    }
+    public function post(Request $request){
+        if($request->has('next')){
+            $month = $this->month + 1;
+        } else if($request->has('prv')){
+            $month = $this->month - 1;
+        }
+        redirect('index');
     }
 
 
